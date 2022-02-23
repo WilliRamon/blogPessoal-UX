96fb29f03a5d599fea35367dbbf5e84cc499623b
WilliRamon <acdc.ramon@hotmail.com>
2022-02-23 12:24:55 -0300

    Iníco do consumo de API

--- src/app/cadastrar/cadastrar.component.ts
+++ src/app/cadastrar/cadastrar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Usuario } from '../model/Usuario';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-cadastrar',
@@ -7,9 +10,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarComponent implements OnInit {
 
-  constructor() { }
+  usuario: Usuario = new Usuario //Variavel sempre ficam em cima dos construtores 
+  confirmarSenha: string 
+  tipoUsuario: string
+  
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit() {     //ngOnInit significa que a página deve realizar algum comando quando ele inicializar
+    window.scroll(0,0)
+
+  }
+
+  confirmSenha(event: any){
+    this.confirmarSenha = event.target.value
+  }
+
+  tipoUser(event: any){
+    this.tipoUsuario = event.target.value
+  }
+
+  cadastrar(){
+    this.usuario.tipo = this.tipoUsuario
+
+    if(this.usuario.senha != this.confirmarSenha){
+      alert('As senha estão incorretas.')
+    }else{
+      this.authService.cadastrar(this.usuario).subscribe((resp: Usuario) => {
+        this.usuario = resp
+        this.router.navigate(['/entrar'])
+        alert('Usuario cadastrado com sucesso!')
+      })
+    }
   }
 
 }
---
+++ src/app/model/Tema.ts
@@ -0,0 +1,7 @@
+import { Postagem } from "./Postagem"
+
+export class Tema{
+    public id: number
+    public descicao: string
+    public postagem: Postagem[]
+}
