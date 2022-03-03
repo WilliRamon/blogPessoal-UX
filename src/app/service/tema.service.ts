fa9775d22e0bcdd8eb3076c1e078c78a1f87603b
WilliRamon <acdc.ramon@hotmail.com>
2022-03-03 04:54:24 -0300

    Criando o método post em Tema

---
+++ src/app/service/tema.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
+import { Tema } from '../model/Tema';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TemaService {
+
+  constructor(private http: HttpClient) { } //Para ter acesso ao meus métodos HTTP
+
+  token ={
+    headers: new HttpHeaders().set('Authorization', environment.token) //Esse campo só terá acesso se tiver esse token
+  }
+
+  getAllTema(): Observable<Tema[]>{
+    return this.http.get<Tema[]>('https://blogpessoalwilliramon.herokuapp.com/tema', this.token) //O mesmo token que adicionei no header
+  }
+
+  postTema(tema: Tema): Observable<Tema>{
+    return this.http.post<Tema>('https://blogpessoalwilliramon.herokuapp.com/tema', tema, this.token)
+  }
+}
